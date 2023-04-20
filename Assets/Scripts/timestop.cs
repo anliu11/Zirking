3c3d0eff4287566beb22163673e9e767c5689f37
USBcat <122058079+USBcat@users.noreply.github.com>
2023-04-20 13:41:34 -0700

    New scripts + copied ts2

--- Assets/Scripts/timestop.cs
+++ Assets/Scripts/timestop.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class timestop : MonoBehaviour
 {
+    public GameManager gameManager;
+    public Vector3 scaleChange;
+    public LayerMask player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManager.GetComponent<GameManager>().isGameActive == true)
+        {
+            transform.localScale += (scaleChange * Time.deltaTime);
+        }
     }
 }
