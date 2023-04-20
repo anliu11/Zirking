a28ef624d479357bc9ba95ea336f32b3f0899a56
USBcat <122058079+USBcat@users.noreply.github.com>
2023-04-20 13:48:53 -0700

    adding scripts and boss in
    remeber to rename and double check if all varibble are the same

--- Assets/Scripts/timestop2.cs
+++ Assets/Scripts/timestop2.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeStop1 : MonoBehaviour
+public class timestop2 : MonoBehaviour
 {
+    public GameManager gameManager;
+    public float sphereRadius;
+    public float playeralv;
+    int layerId = 6;
+    public LayerMask player;
+    public bool timezoned;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        timezoned = false; 
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        int layerMask = 1 << layerId;
+        if (gameManager.GetComponent<GameManager>().isGameActive == true)
+        {
+            if (Physics.CheckSphere(transform.position, sphereRadius, player))
+            {
+            Debug.Log("zone");
+            timezoned = true;
+            }
+        }
     }
 }
